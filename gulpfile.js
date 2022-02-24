727c73cc85dae79584e759731553ad522aa09ddb
Morkos <mark@offtherecord.com>
2022-02-24 12:27:41 -0800

    Up to 0.1.9

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,79 @@
-const { src, dest, watch, series, parallel } = require('gulp');
-const ts = require('gulp-typescript');
-const templateCache = require('gulp-angular-templatecache');
-const sass = require('gulp-sass')(require('sass'));
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const rename = require('gulp-rename');
-const clean = require('gulp-clean');
-const merge = require('merge-stream');
-const babel = require('gulp-babel');
+const { src, dest, watch, series, parallel } = require("gulp");
+const ts = require("gulp-typescript");
+const templateCache = require("gulp-angular-templatecache");
+const sass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const rename = require("gulp-rename");
+const clean = require("gulp-clean");
+const merge = require("merge-stream");
+const babel = require("gulp-babel");
 
 // for production / build
 function jsTask() {
-    var tsProject = ts.createProject('tsconfig.json');
-    return src('app/**/*.ts')
-        .pipe(
+  var tsProject = ts.createProject("tsconfig.json");
+  return (
+    src("app/**/*.ts")
+      /*.pipe(
             babel({
                 plugins: [
                     ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
                     ['babel-plugin-remove-import-export']
                 ],
             })
-        )
-        .pipe(tsProject())
-        .pipe(concat('otr-ui-shared-components.js'))
-        .pipe(dest('dist'))
-        .pipe(rename({ extname: '.min.js' }))
-        .pipe(uglify())
-        .pipe(dest('dist'));
+        )* DISABLE BABEL because we need the imported modules in pdf-preview and others potentially */
+      .pipe(tsProject())
+      .pipe(concat("otr-ui-shared-components.js"))
+      .pipe(dest("dist"))
+      .pipe(rename({ extname: ".min.js" }))
+      .pipe(uglify())
+      .pipe(dest("dist"))
+  );
 }
 
 function cssTask() {
-    return src('app/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concat('otr-ui-shared-components.css'))
-        .pipe(dest('dist'));
+  return src("app/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("otr-ui-shared-components.css"))
+    .pipe(dest("dist"));
 }
 
 function htmlTask() {
-    return src('app/**/*.html')
-        .pipe(templateCache({
-            filename: 'otr-ui-shared-components.tpls.js',
-            module: 'otr-ui-shared-components.tpls',
-            standalone: true
-        }))
-        .pipe(dest('dist'))
-        .pipe(rename({ extname: '.min.js' }))
-        .pipe(uglify())
-        .pipe(dest('dist'));
+  return src("app/**/*.html")
+    .pipe(
+      templateCache({
+        filename: "otr-ui-shared-components.tpls.js",
+        module: "otr-ui-shared-components.tpls",
+        standalone: true,
+      })
+    )
+    .pipe(dest("dist"))
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(uglify())
+    .pipe(dest("dist"));
 }
 
 function createIndex() {
-    const indexStream = src(['dist/*.js', '!dist/*.min.js'])
-        .pipe(concat('index.js'))
-        .pipe(dest('dist'));
+  const indexStream = src([
+    "dist/otr-ui-shared-components.tpls.js",
+    "dist/otr-ui-shared-components.js",
+    "!dist/*.min.js",
+  ])
+    .pipe(concat("index.js"))
+    .pipe(dest("dist"));
 
-    const minifiedIndexStream = src(['dist/*.min.js'])
-        .pipe(concat('index.min.js'))
-        .pipe(dest('dist'));
+  const minifiedIndexStream = src([
+    "dist/otr-ui-shared-components.tpls.min.js",
+    "dist/otr-ui-shared-components.min.js",
+  ])
+    .pipe(concat("index.min.js"))
+    .pipe(dest("dist"));
 
-    return merge(indexStream, minifiedIndexStream);
+  return merge(indexStream, minifiedIndexStream);
 }
 
 function cleanScripts() {
-    return src('dist')
-        .pipe(clean());
+  return src("dist").pipe(clean());
 }
 
 exports.clean = cleanScripts;
