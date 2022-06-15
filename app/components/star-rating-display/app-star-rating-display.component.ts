d71f19ce7b54b82624d5969d8ce2845111eeec4c
Melissa Schatz-Miller <melissa.scmi@gmail.com>
2022-06-15 10:40:32 -0700

    Add star rating display component (#7)
    * add star rating display component
    
    * BEM-ify star-rating-display scss

---
+++ app/components/star-rating-display/app-star-rating-display.component.ts
@@ -0,0 +1,44 @@
+import angular from 'angular';
+
+interface StarRatingDisplay {
+    color?: string;
+    rating: number;
+}
+
+class StarRatingDisplay implements StarRatingDisplay {
+    // Bindings
+    color?: string;
+    rating!: number;
+
+    // Interface
+    percentage!: string;
+    customColor!: string;
+
+    // Variables
+    VISIBLE_STAR_PCT = 10;
+    INITIAL_STAR_OFFSET = 4;
+
+    $onInit() {
+        const wholeStars = Math.floor(this.rating);
+        const wholeStarsPercentage = wholeStars * 20;
+        const decimalStarPercentage =
+            (this.rating - wholeStars) *
+            (this.VISIBLE_STAR_PCT + this.INITIAL_STAR_OFFSET);
+        this.percentage = wholeStarsPercentage + decimalStarPercentage + '%';
+        this.customColor = this.color || '#ffc715';
+    }
+
+}
+
+const component = {
+    selector: 'appStarRatingDisplay',
+    templateUrl: '/components/star-rating-display/app-star-rating-display.component.html',
+    bindings: {
+        color: '@',
+        rating: '<'
+    },
+    controller: StarRatingDisplay,
+    controllerAs: 'vm'
+};
+
+angular.module('otr-ui-shared-components').component(component.selector, component);
