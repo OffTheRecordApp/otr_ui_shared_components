d4df956b1dde01ce3ac552a317a82a67d9baddaa
Shaun Kent <82499628+SKona306@users.noreply.github.com>
2022-06-29 12:34:46 -0700

    Modify avatar image fallback component (#9)
    * modify app-avatar-fallback-image component to use ngletteravatar when an error is thrown and default user when no name or src is provided
    
    * add regex logic for letter avatar char length, changed file names to app-avatar-fallback-image
    
    * change component class name to avoid confusion on parent applications
    
    * change naming schemes of variables to fit apps standards

---
+++ app/components/avatar-image-fallback/app-avatar-image-fallback.component.ts
@@ -0,0 +1,73 @@
+import angular from 'angular';
+
+interface AvatarImageFallbackBindings {
+    src?: string;
+    avatarClass?: string;
+    name?: string;
+}
+
+class AppAvatarImageFallback implements AvatarImageFallbackBindings {
+    // bindings
+    src?: string;
+    avatarClass?: string;
+    name?: string;
+
+    // interface
+    imageClass!: string;
+    charCount!: number;
+    isLetterAvatarVisible!: boolean;
+
+    constructor(private $element, private $scope) {}
+
+    $onInit() {
+        this.imageClass = this.avatarClass
+            ? this.avatarClass
+            : 'app-avatar-image-fallback__default';
+
+        this.setCharCount();
+        this.setLetterProfileAvatar();
+    }
+
+    //sets ngLetterAvatar character count; max 2 characters
+    setCharCount() {
+        const spacePattern = /\s+/;
+        if (this.name) {
+            this.charCount = spacePattern.test(this.name) ? 2 : 1;
+        }
+    }
+
+    setLetterProfileAvatar() {
+        // Use case: when user does not have profile image or name
+        if (!this.src && !this.name) {
+            const appColors = ['#007BE3', '#FFC715', '#00D6B2', '#FF4848', '#385064'];
+            const defaultFallback = angular
+                .element(document.createElement('i'))
+                .addClass('fas fa-user app-avatar-image-fallback__default-user')
+                .css({
+                    background: appColors[Math.floor(Math.random() * appColors.length)]
+                });
+            this.$element.parent().prepend(defaultFallback);
+            this.$element.remove();
+        } else {
+            this.$element.find('img').bind('error', () => {
+                this.$element[0].children[0].remove();
+                this.isLetterAvatarVisible = true;
+                this.$scope.$apply();
+            });
+        }
+    }
+}
+
+angular.module('otr-ui-shared-components').component('appAvatarImageFallback', {
+    templateUrl:
+        'app/components/avatar-image-fallback/app-avatar-image-fallback.component.html',
+    controller: AppAvatarImageFallback,
+    bindings: {
+        src: '<',
+        avatarClass: '@',
+        name: '@'
+    },
+    controllerAs: 'vm'
+});
+
+AppAvatarImageFallback.$inject = ['$element', '$scope'];
