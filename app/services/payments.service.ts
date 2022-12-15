2fa8100b9ebf5b423c14e6f2c7f1f2999b1e79d0
Shaun Kent <shaunkent81@gmail.com>
2022-12-15 15:43:26 -0800

    modify styles credit card form, modify image paths

--- app/services/payments.service.ts
+++ app/services/payments.service.ts
@@ -4,37 +4,37 @@ export class PaymentService {
     public availablePaymentLogos = [
         {
             key: 'visa',
-            logo: 'assets/img/logos/payment-logo-visa.png',
+            logo: '../assets/img/logos/payment-logo-visa.png',
             humanReadableKey: 'Visa'
         },
         {
             key: 'master_card',
-            logo: 'assets/img/logos/payment-logo-master_card.svg',
+            logo: '../assets/img/logos/payment-logo-master_card.svg',
             humanReadableKey: 'Master Card'
         },
         {
             key: 'discover',
-            logo: 'assets/img/logos/payment-logo-discover.jpg',
+            logo: '../assets/img/logos/payment-logo-discover.jpg',
             humanReadableKey: 'Discover'
         },
         {
             key: 'american_express',
-            logo: 'assets/img/logos/payment-logo-american_express.svg',
+            logo: '../assets/img/logos/payment-logo-american_express.svg',
             humanReadableKey: 'American Express'
         },
         {
             key: 'diners_club',
-            logo: 'assets/img/logos/payment-logo-diners_club.jpg',
+            logo: '../assets/img/logos/payment-logo-diners_club.jpg',
             humanReadableKey: `Diner's Club`
         },
         {
             key: 'jcb',
-            logo: 'assets/img/logos/payment-logo-jcb.jpg',
+            logo: '../assets/img/logos/payment-logo-jcb.jpg',
             humanReadableKey: 'JCB'
         },
         {
             key: 'union_pay',
-            logo: 'assets/img/logos/payment-logo-union_pay.jpg',
+            logo: '../assets/img/logos/payment-logo-union_pay.jpg',
             humanReadableKey: 'Union Pay'
         }
     ];
