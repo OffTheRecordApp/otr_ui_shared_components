4821df7cef4daa4695bcb987f66d12198db85858
tigertiger <melissa.scmi@gmail.com>
2023-07-28 15:39:18 -0700

    update multi-select to use ids for selection

--- app/tests/manual/router.ts
+++ app/tests/manual/router.ts
@@ -283,21 +283,25 @@ angular
                                         group: 'Regions',
                                         set: [
                                             {
+                                                id: 13,
                                                 name: 'WEST',
                                                 value: 'WEST',
                                                 label: 'West'
                                             },
                                             {
+                                                id: 14,
                                                 name: 'EAST',
                                                 value: 'EAST',
                                                 label: 'East'
                                             },
                                             {
+                                                id: 15,
                                                 name: 'SOUTH',
                                                 value: 'SOUTH',
                                                 label: 'South'
                                             },
                                             {
+                                                id: 16,
                                                 name: 'MIDDLE',
                                                 value: 'MIDDLE',
                                                 label: 'Mid'
